c78b3fc062254f1e7fcbe36292f1162b9796f762
gerbii <gerbithiago@gmail.com>
2024-08-10 20:33:29 -0300

    Ordenação com Map

---
+++ src/OrdenacaoMap/Evento.java
@@ -0,0 +1,29 @@
+package OrdenacaoMap;
+
+public class Evento {
+	private String nome;
+	private String atracao;
+	
+	public Evento(String nome, String atracao) {
+		this.nome = nome;
+		this.atracao = atracao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getAtracao() {
+		return atracao;
+	}
+
+	
+	@Override
+	public String toString() {
+		return " [nome = " + nome + ", atracao = " + atracao + "]";
+	}
+	
+	
+	
+
+}
